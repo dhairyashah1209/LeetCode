1659275eae970ccd41b14ab7f7f575b37db912c3
Dhairya Shah <dhairyashah1209@gmail.com>
2020-02-09 19:33:30 +0530

    Final Commit

--- LinkedList/sortList.cpp
+++ LinkedList/sortList.cpp
@@ -1,2 +1,48 @@
-#include<bits/stdc++.h>
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    
+    ListNode* mergeSort(ListNode* a, ListNode* b){
+    
+        ListNode* res = NULL;
+        if(a==NULL)
+            return b;
+        if(b==NULL)
+            return a;
+        
+        if(a->val <= b->val){
+            res = a;
+            res->next = mergeSort(a->next,b);
+        }
+        else{
+            res = b;
+            res->next = mergeSort(a,b->next);
+        }
+        return res;
+    }
+    
+    ListNode* sortList(ListNode* head) {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        ListNode *pre = NULL;
+        
+        if(head==NULL || head->next==NULL)
+            return head;
+        
+        while(fast && fast->next){
+            pre = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        pre->next = NULL;
+        
+        return mergeSort(sortList(head),sortList(slow));
+    }
+};
